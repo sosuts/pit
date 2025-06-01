9024e17a0787909485ada1b216b925fbd091dd48
sosuts <sosuke.utsunomiya@gmail.com>
2025-06-01 22:34:01 +0900

    feat: support symlink in get_file_permission

--- src/pit/util.py
+++ src/pit/util.py
@@ -1,9 +1,12 @@
 import os
 
 
-def get_file_permission(file_path):
+def get_file_permission(file_path: str) -> str:
     """ファイルの権限を数字として取得する。ディレクトリなら40000を返す"""
     if os.path.isdir(file_path):
-        return 40000
+        return "40000"
+    # if symlink
+    elif os.path.islink(file_path):
+        return "120000"  # symlink mode in git
     mode = os.stat(file_path).st_mode
-    return int(oct(mode)[2:])
+    return str(oct(mode)[2:])
