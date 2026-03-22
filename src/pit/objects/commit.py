4286d9f3ea30ca1780215282e17e2ebd96ec8a32
sosuts <sosuke.utsunomiya@gmail.com>
2026-03-22 10:33:26 +0900

    feat: enhance Commit class with timestamp and timezone handling; add type hints support

--- src/pit/objects/commit.py
+++ src/pit/objects/commit.py
@@ -2,14 +2,33 @@ import hashlib
 from datetime import datetime
 from string import Template
 
+from pit.objects.tree import Tree
+
 
 class Commit:
-    def __init__(self, tree, parents, author, committer, message):
-        self.tree = tree
+    def __init__(
+        self,
+        tree: Tree,
+        parents: list[str] | None = None,
+        author: str = "",
+        committer: str = "",
+        message: str = "",
+        timestamp: int | None = None,
+        timezone: str | None = None,
+    ):
+        self.tree: Tree = tree
         self.parents = parents
         self.author = author
         self.committer = committer
         self.message = message
+        if timestamp is not None:
+            self.__timestamp = timestamp
+        else:
+            self.__timestamp = int(datetime.now().timestamp())
+        if timezone is not None:
+            self.__timezone = timezone
+        else:
+            self.__timezone = datetime.now().astimezone().strftime("%z")
 
     def template(self) -> Template:
         """
@@ -22,15 +41,13 @@ class Commit:
         """
         Commitオブジェクトの内容を返す。
         """
-        timestamp = int(datetime.now().timestamp())
-        timezone = datetime.now().astimezone().strftime("%z")
         parents_str = "\n".join(self.parents) if self.parents else ""
         return (
-            f"tree {self.tree}\n"
-            f"parent {parents_str}\n"
-            f"author {self.author} {timestamp} {timezone}\n"
-            f"committer {self.committer} {timestamp} {timezone}\n\n"
-            f"{self.message}\n"
+            f"tree {self.tree.hash}\n"
+            + (f"parent {parents_str}\n" if self.parents else "")
+            + f"author {self.author} {self.__timestamp} {self.__timezone}\n"
+            + f"committer {self.committer} {self.__timestamp} {self.__timezone}\n\n"
+            + f"{self.message}\n"
         )
 
     @property
@@ -47,11 +64,18 @@ class Commit:
         """
         Gitと同じようにコミットオブジェクトの文字列表現を返す。
         """
+        if self.parents:
+            parents_str = "\n".join(f"parent {p}" for p in self.parents)
+        else:
+            parents_str = ""
         return (
-            f"tree {self.tree}\n"
-            f"parent {self.parents}\n"
-            f"author {self.author}\n"
-            f"committer {self.committer}\n"
+            f"tree {self.tree.hash}\n"
+            + parents_str
+            + f"author {self.author} {self.__timestamp} {self.__timezone}\n"
+            + f"committer {self.committer} {self.__timestamp} {self.__timezone} \n"
             f"\n"
             f"{self.message}\n"
         )
+
+    def __repr__(self) -> str:
+        return self.__str__()
--- tests/test_commit.py
+++ tests/test_commit.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from datetime import datetime
 from pathlib import Path
 
 from pit.objects.commit import Commit
@@ -7,8 +8,10 @@ from pit.objects.tree import Tree
 
 
 class TestCommit:
-    def test_dir_hash(self, tmp_path, freezer):
-        freezer.move_to("2025-12-01 12:34:56")
+    GIT_AUTHOR_DATE = int(datetime(2025, 12, 1, 12, 34, 56).timestamp())
+    GIT_COMMITTER_DATE = int(datetime(2025, 12, 1, 12, 34, 56).timestamp())
+
+    def test_tree_hash_when_time_is_fixed(self, tmp_path):
         """一時的なgitリポジトリでGitと同じハッシュ値になるか確認する"""
 
         # 一時ディレクトリにリポジトリを作成
@@ -23,12 +26,17 @@ class TestCommit:
         subprocess.run(["git", "add", "-A"], check=True)
 
         # Gitのツリーオブジェクトを生成
+        env = os.environ.copy()
+        env["GIT_AUTHOR_DATE"] = str(self.GIT_AUTHOR_DATE)
+        env["GIT_COMMITTER_DATE"] = str(self.GIT_COMMITTER_DATE)
         subprocess.run(
             ["git", "commit", "-m", "test commit"],
             capture_output=True,
             check=True,
+            env=env,
         )
-        # get commit hash
+
+        # ツリーのハッシュを取得
         git_hash = subprocess.run(
             ["git", "rev-parse", "HEAD"],
             capture_output=True,
@@ -36,12 +44,19 @@ class TestCommit:
             check=True,
         ).stdout.strip()
 
+        # Pitのツリーを作成
         tree_ = Tree.from_directory(str(repo_path))
+        # self.GIT_AUTHOR_DATEとself.GIT_COMMITTER_DATEをintに変換して渡す
         commit = Commit(
             tree=tree_,
-            parents=[git_hash],
             author="sosuts <sosuke.utsunomiya@gmail.com>",
             committer="sosuts <sosuke.utsunomiya@gmail.com>",
             message="test commit",
+            timestamp=self.GIT_AUTHOR_DATE,
+            timezone=datetime.fromtimestamp(self.GIT_AUTHOR_DATE)
+            .astimezone()
+            .strftime("%z"),
         )
-        print(commit.content)
+        assert commit.hash == git_hash, (
+            f"Commit hashes do not match! {commit.hash} != {git_hash}"
+        )
